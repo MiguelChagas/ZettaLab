60c2e66a47d6b60da3d474348b5779614dd884df
MiguelChagas <108590753+MiguelChagas@users.noreply.github.com>
2025-10-16 17:35:57 -0300

    new: Estilizando os primeiros passos da header e footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { FooterComponent } from './components/layout/footer/footer.component';
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ZettaLab';
