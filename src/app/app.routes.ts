71c4b129681c560172bcfe14d48c95bace9853c8
MiguelChagas <108590753+MiguelChagas@users.noreply.github.com>
2025-10-17 17:19:31 -0300

    new: Adicionando a primeira página de países

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CountryListComponent } from './pages/country-list/country-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/paises',
+    pathMatch: 'full',
+  },
+  {
+    path: 'paises',
+    component: CountryListComponent,
+  },
+];
