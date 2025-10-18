e3e4e5d3fb5beac456241e47b63cb3dd5e1926ef
MiguelChagas <108590753+MiguelChagas@users.noreply.github.com>
2025-10-18 20:27:38 -0300

    new: Adicionando nova página principal dos países

--- src/app/pages/country-list/country-list.component.ts
+++ src/app/pages/country-list/country-list.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-country-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './country-list.component.html',
-  styleUrl: './country-list.component.scss'
+  styleUrl: './country-list.component.scss',
 })
-export class CountryListComponent {
+export class CountryListComponent implements OnInit {
+  countries: any[] = [];
+  isLoading: boolean = true;
+  errorMessage: string | null = null;
 
+  constructor(private countryService: CountryService) {}
+
+  ngOnInit(): void {
+    console.log('Iniciando a busca por países...');
+    this.countryService.getAllCountries().subscribe({
+      next: (data) => {
+        console.log('Dados recebidos com sucesso!', data);
+        this.countries = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Ocorreu um erro ao buscar os países:', err);
+        this.errorMessage =
+          'Não foi possível carregar a lista de países. Tente novamente mais tarde.';
+        this.isLoading = false;
+      },
+    });
+  }
 }
---
+++ src/app/services/country.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CountryService {
+  private apiUrl = 'https://restcountries.com/v3.1';
+
+  constructor(private http: HttpClient) {}
+
+  getAllCountries(): Observable<any[]> {
+    const fields = 'name,flags,population,capital';
+
+    return this.http.get<any[]>(`${this.apiUrl}/all?fields=${fields}`);
+  }
+}
